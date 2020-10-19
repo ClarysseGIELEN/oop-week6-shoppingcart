fc51fbb32f157bd68864cba19016f993d846dc21
ClarysseGIELEN23394 <71280890+ClarysseGIELEN@users.noreply.github.com>
2020-10-19 16:25:13 +0200

    Add files via upload

---
+++ Fruit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_week6_shoppingcart
+{
+    public struct Fruit
+    {
+        public double Cost;
+
+        public Fruit(double cost)
+        {
+            this.Cost = cost;
+        }
+    }
+}
