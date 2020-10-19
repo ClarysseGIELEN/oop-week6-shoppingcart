1e867fd1a2513c86141d7b31aa87f42854ee349f
Clarysse <clarysse.gielen@edu.devinci.fr>
2020-10-19 17:28:15 +0200

    first changes

--- oop-week6-shoppingcart/Fruit.cs
+++ oop-week6-shoppingcart/Fruit.cs
@@ -7,7 +7,7 @@ namespace oop_week6_shoppingcart
     public class Fruit : Product
     {
         public double Cost;
-
+        //
         public Fruit(double cost)
         {
             this.Cost = cost;
